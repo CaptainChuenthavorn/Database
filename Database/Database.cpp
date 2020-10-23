c24ce37f9832ab2183c6a58648362ac8f8a9fef6
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-23 21:33:04 +0700

    finish

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,60 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 struct STUDENT {
 	char name[50];
 	int score;
 	char tempName[50];
-	//int Pr[50];//Percentile
+	int Pr;//Percentile
+	int	pos;//Position
 };
 
 int main() {
 	struct STUDENT* students;
-	int size;
+	int n;
 	int i, j,temp;
-	
-	scanf("%d", &size);
-	students = (struct STUDENT*)malloc (size * sizeof(struct STUDENT));
-	for (int i = 0;i < size; i++) {
+	scanf("%d", &n);
+	students = (struct STUDENT*)malloc (n * sizeof(struct STUDENT));
+	for (int i = 0;i < n; i++) {
 		printf("Enter name :");
 		scanf("%s", students[i].name);
-		printf("Enter tempName :");
-		scanf("%s", students[i].tempName);
-		
 	}
-	for (int i = 0;i < size; i++) {
+	for (int i = 0;i < n; i++) {
 		printf("Enter score :");
 		scanf("%d", &students[i].score);
 	}
-
-	for (i = 0; i < size; ++i)
+	
+	for (i = 0; i < n; i++)
 	{
-		for (j = i + 1; j < size; ++j)
+		for (j = i + 1; j < n; j++)
 		{
 			if (students[i].score > students[j].score)
 			{
 				temp = students[i].score;
 				students[i].score = students[j].score;
 				students[j].score = temp;
-				students[i].tempName = students[i].name;
-				students[i].name = students[j].name;
-				students[j].name = students[i].tempName;
+				strcpy(students[i].tempName, students[i].name);
+				strcpy(students[i].name, students[j].name);
+				strcpy(students[j].name, students[i].tempName);
 			}
-
 		}
 	}
-	printf("\n");
-	for (int i = 0;i < size; i++) {
-		
-		//printf("name %d:", i + 1);
-		//printf(" %s ", students[i].name);
-		printf("score %d:",i+1);
-		printf(" %d \n", students[i].score);
-		
+	for (int i = 0;i < n; i++) {
+		students[i].pos = i;
 	}
-
-
-
+	printf("\n");
+	for (int i = 0;i < n; i++) {
+		students[i].Pr = students[i].pos * 100 / n;
+		printf("\n%s has score %d, is in percentile %d", students[i].name, students[i].score, students[i].Pr);
+	}
 	return 0;
 }
